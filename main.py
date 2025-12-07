7bc730540c58792090b14ddde96a737f89810ab3
kKuteKuto <118588942+kKuteKuto@users.noreply.github.com>
2025-12-07 10:31:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def process(latest_version: Version):
 
     print("Downloading patches")
     pikoRelease = download_release_asset(
-        "crimera/piko", "^(piko|patches).*\\.(jar|rvp)$", "bins", "patches.jar",True,"v2.0.0-dev.13"
+        "crimera/piko", "^piko.*jar$", "bins", "patches.jar"
     )
 
     print("Downloading integrations")
